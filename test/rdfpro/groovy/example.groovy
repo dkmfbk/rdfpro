f2b0ba8deb1aae4feda9d0874b97c69808dce6f4
Francesco Corcoglioniti <corcoglio@fbk.eu>
2014-12-11 11:41:34 +0100

    Deleted test

--- test/rdfpro/groovy/example.groovy
+++
@@ -1,6 +0,0 @@
-def init(args) { println "Initialized with " + args; }
-def start(x)   { println "Begin pass " + x; num = 0 }
-def end(x)     { println "End pass " + x + ", " + num + " triples"; emit(iri("ex:stats"), iri("ex:numtriples"), num, null); }
-
-emitIf(p == rdf:type);
-++ num;
